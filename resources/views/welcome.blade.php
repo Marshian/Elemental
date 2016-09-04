e4fb6848e6704c00ce1af5db5201b00c49c245d5
Ryan Shofner <ryan@letsgolearn.com>
2016-09-03 17:39:53 -0700

    small updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Darkstar Mountain Bike Tours</title>
+    <title>Darkstar Mountain Tours</title>
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
