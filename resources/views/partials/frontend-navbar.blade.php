7133a86113c1f37f0cdec75a2fcf0645c6f57603
Ryan Shofner <ryan@letsgolearn.com>
2016-09-05 13:24:31 -0700

    update styles and layouts

--- resources/views/partials/frontend-navbar.blade.php
+++ resources/views/partials/frontend-navbar.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="/img/logo.png" alt="Darkstar Mountain Bike Tours"></a>
+            <a class="navbar-brand" href="#"><img src="/img/logo.png" alt="Dark Star Mountain Bike Tours"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
