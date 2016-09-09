21af7cb09b176aed7331211628770b8fa254e2ba
Ryan Shofner <ryan@letsgolearn.com>
2016-09-09 00:58:19 -0700

    run seed for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,7 +19,11 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
         return $app;
+    }
+
+    public function setUp(){
+        parent::setUp();
+        $this->app[Illuminate\Contracts\Console\Kernel::class]->call('db:seed');
     }
 }
