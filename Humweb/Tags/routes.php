1aea9d7e207a869f1539f3702a0d7e45c28377e6
Ryan Shofner <ryan@letsgolearn.com>
2016-09-09 00:57:58 -0700

    cleanup

--- Humweb/Tags/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// App::bind('Humweb\Pages\Models\PagesInterface','Humweb\Pages\Models\Pages');
-// Route::controller('admin/pages', 'Humweb\Pages\Controllers\AdminPagesController');
-// Route::controller('admin/categories', 'Humweb\Pages\Controllers\AdminCategoriesController');
-
