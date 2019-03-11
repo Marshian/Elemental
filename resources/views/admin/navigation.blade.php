55ebdbab124069b7d6d859588ceac27c741d84f3
Marshian <admin@marshian.com.au>
2019-03-11 18:48:11 +0800

    nav update
    added auth user

--- resources/views/admin/navigation.blade.php
+++ resources/views/admin/navigation.blade.php
@@ -6,7 +6,7 @@
                     <img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}">
                 </span>
                 <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                    <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
+                    <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">{{ Auth::user()->name }}</strong>
                     </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
                     <ul class="dropdown-menu animated fadeInRight m-t-xs">
                         <li><a href="#">Profile</a></li>
