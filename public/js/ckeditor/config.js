39d0c50be64540b380af155f7e39fa02dcd19ceb
Ryun Shofner <ryan@humboldtweb.com>
2019-03-18 16:48:48 -0700

    update ckeditor

--- public/js/ckeditor/config.js
+++ public/js/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
     config.height = 500;
-    config.contentsCss = '/css/app.css';
+    config.contentsCss = ['/frontend/css/style.css','/frontend/css/bootstrap.css','/frontend/css/all.css'];
     // config.extraPlugins = 'showprotected';
     config.uploadUrl = '/uploader/upload.php';
 	// The toolbar groups arrangement, optimized for two toolbar rows.
@@ -58,15 +58,15 @@ CKEDITOR.editorConfig = function( config ) {
     config.format_tags = 'p;h1;h2;h3;h4;h5;h6;pre';
 
     // Protect Blade tags
-    config.protectedSource.push(/{{[\s\S]*?}}/g);
-    config.protectedSource.push(/@[\s\S]*?\([\s\S]*?\)/g);
-    config.protectedSource.push(/{!![\s\S]*?!!}/g);
-    config.protectedSource.push(/\[[\s\S]*?\]/g);
-    config.protectedSource.push(/<\?[\s\S]*?\?>/g); // PHP
-
-    // Allow Empty i tags for font icons
-    config.protectedSource.push(/<i[\s\S]*?\>/g); //allows beginning <i> tag
-    config.protectedSource.push(/<\/i[\s\S]*?\>/g); //allows ending </i> tag
+    // config.protectedSource.push(/{{[\s\S]*?}}/g);
+    // config.protectedSource.push(/@[\s\S]*?\([\s\S]*?\)/g);
+    // config.protectedSource.push(/{!![\s\S]*?!!}/g);
+    // config.protectedSource.push(/\[[\s\S]*?\]/g);
+    // config.protectedSource.push(/<\?[\s\S]*?\?>/g); // PHP
+    //
+    // // Allow Empty i tags for font icons
+    // config.protectedSource.push(/<i[\s\S]*?\>/g); //allows beginning <i> tag
+    // config.protectedSource.push(/<\/i[\s\S]*?\>/g); //allows ending </i> tag
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
